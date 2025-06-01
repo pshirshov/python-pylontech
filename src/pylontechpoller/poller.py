82a1421f4bd5386bf6df3ea646a1b01b14264bf4
Pavel Shirshov <pshirshov@eml.cc>
2025-06-01 17:39:57 +0100

    poller: home assistant pushes

--- src/pylontechpoller/poller.py
+++ src/pylontechpoller/poller.py
@@ -1,31 +1,37 @@
 import argparse
-import datetime
 import json
-import time
-import sys
 import logging
-import itertools
+import sys
+import time
 
-from rich import print_json
 from pylontech import *
-from pymongo import MongoClient
+from pylontechpoller.reporter import MongoReporter, HassReporter
 
 logger = logging.getLogger(__name__)
 
 
 def find_min_max_modules(modules):
     all_voltages = []
+    all_disbalances = []
+
     for module in modules:
-        for voltage in module["CellVoltages"]:
-            all_voltages.append((module["NumberOfModule"], voltage))
+        mid = module["NumberOfModule"]
+        cvs = module["CellVoltages"]
+        for voltage in cvs:
+            all_voltages.append((mid, voltage))
+        vmax = max(cvs)
+        vmin = min(cvs)
+        d = vmax - vmin
+        all_disbalances.append((mid, d))
 
     if not all_voltages:
         return None, None
 
     min_pair = min(all_voltages, key=lambda x: x[1])
     max_pair = max(all_voltages, key=lambda x: x[1])
+    max_disbalance = max(all_disbalances, key=lambda x: abs(x[1]))
 
-    return min_pair, max_pair
+    return min_pair, max_pair, max_disbalance
 
 
 
@@ -48,22 +54,18 @@ def minimize(b: json) -> json:
     modules = b["modules"]
     find_min_max_modules(modules)
 
-    (min_pair, max_pair) = find_min_max_modules(modules)
-    # allcv = list(itertools.chain.from_iterable(map(lambda m: m["CellVoltages"], modules)))
-    # vmin = min(allcv)
-    # vmax = max(allcv)
+    (min_pair, max_pair, max_disbalance) = find_min_max_modules(modules)
 
     return {
         "ts": b["timestamp"],
         "cvmin": min_pair,
         "cvmax": max_pair,
         "stack_disbalance": min_pair[1] - max_pair[1],
+        "max_module_disbalance": max_disbalance,
         "modules": list(map(minimize_module, modules)),
     }
 
-def mongo_cleanup(collection, retention_days):
-    threshold = datetime.datetime.now() - datetime.timedelta(days=retention_days)
-    collection.delete_many({"ts": {"$lt": threshold}})
+
 
 def run(argv: list[str]):
     parser = argparse.ArgumentParser(description="Pylontech RS485 poller")
@@ -75,10 +77,18 @@ def run(argv: list[str]):
     parser.add_argument("--interval", type=int, help="polling interval in msec", default=1000)
     parser.add_argument("--retention-days", type=int, help="how long to retain history data", default=90)
     parser.add_argument("--debug", type=bool, help="verbose output", default=False)
-    parser.add_argument("--mongo-url", type=str, help="mongodb url", default=False)
+
+    parser.add_argument("--mongo-url", type=str, help="mongodb url", default=None)
     parser.add_argument("--mongo-db", type=str, help="target mongo database", default="pylontech")
     parser.add_argument("--mongo-collection-history", type=str, help="target mongo collection_hist for stack history", default="history")
     parser.add_argument("--mongo-collection-meta", type=str, help="target mongo collection_hist for stack data", default="meta")
+
+    parser.add_argument("--hass-url", type=str, help="hass url", default=None)
+    parser.add_argument("--hass-stack-disbalance", type=str, help="state id", default="input_number.stack_disbalance")
+    parser.add_argument("--hass-max-battery-disbalance", type=str, help="state id", default="input_number.max_bat_disbalance")
+    parser.add_argument("--hass-max-battery-disbalance-id", type=str, help="state id", default="input_text.max_disbalance_id")
+    parser.add_argument("--hass-token-file", type=str, help="hass token file", default="/var/run/agenix/hass-token")
+
 
     args = parser.parse_args(argv[1:])
 
@@ -88,25 +98,42 @@ def run(argv: list[str]):
     cc = 0
     spinner = ['|', '/', '-', '\\']
 
+    reporters = []
+
     while True:
         try:
             logging.debug("Preparing client...")
             p = Pylontech(ExscriptTelnetTransport(host=args.source_host, port=args.source_port, timeout=args.timeout))
-            
-            mongo = MongoClient(args.mongo_url)
-            db = mongo[args.mongo_db]
 
-            collection_meta = db[args.mongo_collection_meta]
+            mongo_url = args.mongo_url
 
-            collection_hist = db[args.mongo_collection_history]
-            collection_hist.create_index("ts", expireAfterSeconds=3600*24*90)
+            if mongo_url:
+                reporters.append(MongoReporter(
+                    mongo_url,
+                    args.mongo_db,
+                    args.mongo_collection_meta,
+                    args.mongo_collection_history,
+                    args.retention_days
+                ))
+
+            hass_url = args.hass_url
+            print(hass_url)
+            if hass_url:
+                reporters.append(HassReporter(
+                    hass_url,
+                    args.hass_stack_disbalance,
+                    args.hass_max_battery_disbalance,
+                    args.hass_max_battery_disbalance_id,
+                    args.hass_token_file
+                ))
 
             logging.info("About to start polling...")
             bats = p.scan_for_batteries(2, 10)
 
             logging.info("Have battery stack data")
-            collection_meta.insert_one({'ts':  datetime.datetime.now().isoformat(), "stack": to_json_serializable(bats)})
 
+            for reporter in reporters:
+                reporter.report_meta(bats)
 
             for b in p.poll_parameters(bats.range()):
                 cc += 1
@@ -115,11 +142,14 @@ def run(argv: list[str]):
                     sys.stdout.write('\r' + spinner[cc % len(spinner)])
                     sys.stdout.flush()
 
+                mb = minimize(b)
                 # print(print_json(json.dumps(minimize(b))))
-                collection_hist.insert_one(minimize(b))
+                for reporter in reporters:
+                    reporter.report_state(mb)
 
                 if cc % 86400 == 0:
-                    mongo_cleanup(collection_hist, args.retention_days)
+                    for reporter in reporters:
+                        reporter.cleanup()
 
                 time.sleep(args.interval / 1000.0)
         except (KeyboardInterrupt, SystemExit):
